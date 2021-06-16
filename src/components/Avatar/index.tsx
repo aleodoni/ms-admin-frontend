47ed13b5cf6aad61a2f4c1d27d657612f232a454
aleodoni <aleodoni@gmail.com>
2021-06-16 18:21:09 -0300

    feat: Create initial avatar component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Avatar: React.FC = () => {
+  return (
+    <div className="flex items-center justify-around px-20 h-full text-lg w-auto text-gray-100 bg-transparent">
+      <span>avatar</span>
+    </div>
+  );
+};
+
+export default Avatar;
