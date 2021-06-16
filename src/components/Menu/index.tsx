6527f43f005488843b5c7a49de754cdd78208ba7
aleodoni <aleodoni@gmail.com>
2021-06-16 18:21:40 -0300

    feat: Add Link into menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import Link from 'next/link';
+
+import Avatar from '../Avatar';
 
 const Menu: React.FC = () => {
   return (
     <>
       <div className="flex items-center justify-between w-full h-24 bg-dark-blue px-8">
-        <span className="flex text-4xl items-center text-gray-100 font-bold w-56 h-full">
-          Admin page
-        </span>
+        <Link href="/welcome">
+          <a className="flex text-4xl items-center text-gray-100 font-bold w-56 h-full">
+            Admin page
+          </a>
+        </Link>
         <div className="flex items-center justify-around px-10 text-lg w-2/5 h-full">
-          <span className="text-gray-100">Users</span>
-          <span className="text-gray-100">Systems</span>
-          <span className="text-gray-100">Roles</span>
-          <span className="text-gray-100">Permissions</span>
+          <Link href="#">
+            <a className="text-gray-100">Users</a>
+          </Link>
+          <Link href="#">
+            <a className="text-gray-100">Systems</a>
+          </Link>
+          <Link href="#">
+            <a className="text-gray-100">Roles</a>
+          </Link>
+          <Link href="#">
+            <a className="text-gray-100">Permissions</a>
+          </Link>
         </div>
-        <div className="flex items-center justify-around px-20 h-full text-lg w-auto text-gray-100 bg-dark-blue">
-          <span>avatar</span>
-        </div>
+        <Avatar />
       </div>
     </>
   );
